768e6855d2b947f157e4b0e21ac45ee11d79e2d1
Felix Norton <felix@succinctdesign.co.za>
2014-01-16 13:55:10 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SuccinctSite::Application.config.secret_key_base = 'aef9e181eb7b93f39457a873134bc46f2bec150ffe03729ba5a9e6a9d1f63f9c724b7170350c7b525703cea225ac68d63af03dfd1561c02e402446e422a5204d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SuccinctSite::Application.config.session_store :cookie_store, key: '_SuccinctSite_session'
